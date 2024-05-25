4837d195cba0157f9a4fb0825161ba24afd318d5
aelitaa <gunel.sultanova@outlook.com>
2024-05-25 20:40:20 +0400

    1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,8 +1,15 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const input = data.toString().trim();
-  console.log(`Your name is: ${input}`);
-  console.log('This important software is now closing');
-  process.exit();
+process.stdin.resume();
+
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
